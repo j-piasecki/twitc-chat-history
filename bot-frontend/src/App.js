af26bac18433b8a34946da8391bff9eb37c68e97
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-05-21 18:43:27 +0200

    Add router

--- bot-frontend/src/App.js
+++ bot-frontend/src/App.js
@@ -1,10 +1,12 @@
 
+import { Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Bot
+      <Link to="/test">Test</Link>
     </div>
   );
 }
