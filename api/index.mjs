4bb785c175a1eb5a85ff5db924e5125f57086b55
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-05-21 23:40:46 +0200

    Move express setup

--- api/index.mjs
+++ api/index.mjs
@@ -1,6 +1,37 @@
-export { getChannelId } from './getChannelId.mjs';
-export { getMessagesInChannel } from './getMessagesInChannel.mjs';
-export { getMessagesInChannelForUser } from './getMessagesInChannelforUser.mjs';
-export { getUserChannels } from './getUserChannels.mjs';
-export { getUserId } from './getUserId.mjs';
-export { getUserName } from './getUserName.mjs';
+import express from "express";
+import cors from "cors";
+import http from "http";
+import https from "https";
+
+import { getChannelId } from './getChannelId.mjs';
+import { getMessagesInChannel } from './getMessagesInChannel.mjs';
+import { getMessagesInChannelForUser } from './getMessagesInChannelforUser.mjs';
+import { getUserChannels } from './getUserChannels.mjs';
+import { getUserId } from './getUserId.mjs';
+import { getUserName } from './getUserName.mjs';
+
+//const key = fs.readFileSync("certs/kew.key", "utf-8");
+//const crt = fs.readFileSync("certs/crt.crt", "utf-8");
+//const credentials = { key: key, cert: crt };
+
+const app = express();
+app.use(cors());
+
+export function setupAPI() {
+    app.get("/channelId/:channelName", getChannelId);
+
+    app.get("/userId/:userName", getUserId);
+
+    app.get("/userName/:userId", getUserName);
+
+    app.get("/channel/:channel", getMessagesInChannel);
+
+    app.get("/channel/:channel/user/:user", getMessagesInChannelForUser);
+
+    app.get("/userChannels/:user", getUserChannels);
+
+    let httpServer = http.createServer(app);
+    //let httpsServer = https.createServer(credentials, app);
+    httpServer.listen(8080);
+    //httpsServer.listen(443);
+}
--- main.mjs
+++ main.mjs
@@ -1,11 +1,10 @@
 import { connect } from "./bot.mjs";
 import { initDB, saveMessage } from './db.mjs';
-import { setupAPI } from './api.mjs';
+import { setupAPI } from './api/index.mjs';
 
 await initDB();
 setupAPI();
 
-// connect((channel, user, msg) => {
-//     saveMessage(msg, user["room-id"], channel, user["user-id"], user["username"]);
-//     //console.log(msg);
-// });
+connect((channel, user, msg) => {
+    saveMessage(msg, user["room-id"], channel, user["user-id"], user["username"]);
+});
