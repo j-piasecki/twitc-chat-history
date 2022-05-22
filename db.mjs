61a5933002b5dc3966da8a9eb6bc3e0cf4a20f6d
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-05-22 22:31:22 +0200

    Fix message counting

--- db.mjs
+++ db.mjs
@@ -101,7 +101,7 @@ export async function saveMessage(message, channelId, channelName, userId, userN
     ]);
 
     pool.query(`
-        INSERT INTO user_channels (user_id, channel_id) VALUES ($1, $2) ON CONFLICT (user_id, channel_id) DO UPDATE SET count=user_channels.count + 1
+        INSERT INTO user_channels (user_id, channel_id, count) VALUES ($1, $2, 1) ON CONFLICT (user_id, channel_id) DO UPDATE SET count=user_channels.count + 1
     `, [userId, channelId]);
 
     pool.query(`
